edced6c6bde882ff96002ffffca0341dc8c2cae3
jonatan.trevisan <jonatan.trevisan@addoc.com>
2021-11-07 13:26:07 -0300

    Subiendo Desafio Clase 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles.css';
+import {NavBar} from './NavBar/NavBar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+        <NavBar/>
+
+      <section className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Learn React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </section>
     </div>
   );
 }
---
+++ src/Component/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.scss';
+
+export const NavBar = () => {
+
+    return (
+        <header classname="header">
+
+            <h1>El Jaugar</h1>
+
+            <nav>
+                <ul>
+                    <li>Inicio</li>
+                    <li>Nosotros</li>
+                    <li>E-comerse</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
