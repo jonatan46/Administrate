3369a1395bdb5f05fd9e21dc74e9133473ea6a50
jonatan.trevisan <jonatan.trevisan@addoc.com>
2021-11-22 22:39:06 -0300

    acomodando un poco el NavBar

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -7,16 +7,14 @@ export const NavBar = () => {
     return (
         <header className="header">
 
-            <h1>El Jaugar</h1>
+            <Link to="/"><h1>El Jaugar</h1></Link>
 
             <nav>
                 <ul>
-                    <li><Link to="/">Inicio</Link></li>
                     <li><Link to="listcomplet">E-comerse</Link></li>
                     <li><Link to="list/Encendido">Bujias</Link></li>
                     <li><Link to="list/luces">Opticas</Link></li>
                     <li><Link to="list/agua">Refrigeracion</Link></li>
-                    <li><Link to="contact">Contacto</Link></li>
                 </ul>
             </nav>
             <Link to="/carrito"><CartWidget/></Link>
